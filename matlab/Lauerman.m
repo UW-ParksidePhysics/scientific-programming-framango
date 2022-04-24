8712d557d7c37eea0bc1596bbcd6e7afec83a340
Frank <98779079+Framango@users.noreply.github.com>
2022-04-24 10:34:19 -0500

    Update Lauerman.m

--- matlab/Lauerman.m
+++ matlab/Lauerman.m
@@ -1,4 +1,4 @@
-%Goal is to plot a single graph of probability of a two state system, then later multiple graphs.
+%Goal is to plot a single graph of probability of a two state system.
 %Probality equals the multiplicity of the macrostate over the muliplicty of the entire system, which is in line 43
 %N is the total amount of chances of the system and n being the secondary amount.(ex. total amount of coins flipped is 100(N), with 20 head and 80(n) tails.)
 % multiplicity of the entire system is calculated use 2^N, 2 being there because its a two state system and N being the total amount of coins flipped.
