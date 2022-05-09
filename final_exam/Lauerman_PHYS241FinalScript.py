607bf12d6218b401f09e4217b7da7b5d6d10b3cd
framango <framango99@gmail.com>
2022-05-09 16:39:58 -0500

    First day of work(in class)

--- final_exam/Lauerman_PHYS241FinalScript.py
+++ final_exam/Lauerman_PHYS241FinalScript.py
@@ -1,1 +1,14 @@
+from two_column_text_read import two_column_text_read
+
+filename = 'Al.Fm-3m.GGA-PBE.volumes_energies.dat'
+
+
+def parse_file_name(filename):
+    x = filename.split(".")
+    chemical_symbol = x[0]
+    crystal_symmetry_symbol = x[1]
+    density_functional_exchange = x[2]
+    return chemical_symbol, crystal_symmetry_symbol, density_functional_exchange
+
+
 
--- final_exam/generate_matrix.py
+++ final_exam/generate_matrix.py
@@ -13,18 +13,18 @@ def generate_matrix(minimum_x, maximum_x, number_of_dimensions, potential_name, 
     hbar = 1.0
     mass = 1.0
 
-    grid_spacing = (maximum_x - minimum_x)/(number_of_dimensions - 1)
-    units_prefactor = hbar**2 / (2 * mass * grid_spacing**2)
+    grid_spacing = (maximum_x - minimum_x) / (number_of_dimensions - 1)
+    units_prefactor = hbar ** 2 / (2 * mass * grid_spacing ** 2)
 
     horizontal_grid = np.linspace(minimum_x, maximum_x, num=number_of_dimensions)
     if potential_name == 'harmonic':
-        angular_frequency = potential_parameter * hbar / (mass * (maximum_x - minimum_x)**2)
+        angular_frequency = potential_parameter * hbar / (mass * (maximum_x - minimum_x) ** 2)
         reduced_potential = 0.5 * mass * np.power(angular_frequency * horizontal_grid, 2) / units_prefactor
 
     elif potential_name == 'sinusoidal':
         well_width = (maximum_x - minimum_x)
         wave_vector = np.pi / well_width
-        pre_factor = 5 * potential_parameter * hbar**2 / (2. * mass * well_width**2)
+        pre_factor = 5 * potential_parameter * hbar ** 2 / (2. * mass * well_width ** 2)
         reduced_potential = pre_factor * np.sin(wave_vector * horizontal_grid) / units_prefactor
 
     elif potential_name == 'square':
@@ -34,20 +34,20 @@ def generate_matrix(minimum_x, maximum_x, number_of_dimensions, potential_name, 
         number_of_outside_points = int(number_of_dimensions / 4.)
         reduced_potential = horizontal_grid
         reduced_potential[0:number_of_outside_points] = well_depth
-        reduced_potential[number_of_well_points+number_of_outside_points:] = well_depth
+        reduced_potential[number_of_well_points + number_of_outside_points:] = well_depth
         reduced_potential /= units_prefactor
 
     else:
         horizontal_grid = np.linspace(minimum_x, maximum_x, num=number_of_dimensions)
         reduced_potential = 0. * horizontal_grid
 
-    off_diagonal_terms_array = -1. * np.ones(number_of_dimensions-1)
+    off_diagonal_terms_array = -1. * np.ones(number_of_dimensions - 1)
     diagonal_terms_array = np.full(number_of_dimensions, 2) + reduced_potential
 
     matrix_one = np.diagflat(off_diagonal_terms_array, -1)
     matrix_two = np.diagflat(diagonal_terms_array)
     matrix_three = np.diagflat(off_diagonal_terms_array, 1)
 
-    matrix_total = units_prefactor*(matrix_one + matrix_two + matrix_three)
+    matrix_total = units_prefactor * (matrix_one + matrix_two + matrix_three)
 
     return matrix_total
