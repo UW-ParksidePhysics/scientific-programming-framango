514d14e0866b508a1f517f6c593510dc4af213f2
framango <framango99@gmail.com>
2022-05-11 17:08:53 -0500

    Look at dis graph

--- final_exam/Lauerman_PHYS241FinalScript.py
+++ final_exam/Lauerman_PHYS241FinalScript.py
@@ -38,23 +38,22 @@ equilibrium_volume = fit_parameters[3]
 
 
 def annotate_graph(chemical_symbol, crystal_symmetry):
-    bulk_modulus_gpa = bulk_modulus
-    ax.annotate(chemical_symbol, xy=(130, 0))
+    ax.annotate(chemical_symbol, xy=(0, 0))
 
     ax.annotate(r'$ {}\overline{{{}}} {}$'.format(crystal_symmetry[0:2],
                                                   crystal_symmetry[3],
                                                   crystal_symmetry[1]),
-                xy=(115, 0))
+                xy=(0, -200))
 
     ax.annotate('K_0={:.6f}GPa'.format(bulk_modulus_gpa),
-                xy=(115, -0.001))
+                xy=(0, -400))
 
-    ax.annotate('V_0={:.3f}A^3/atom'.format(equilibrium_volume),
-                xy=(115, -0.001))
-    plt.axvline(equilibrium_volume - array_2[0][array_2[1].index(min(array_2[1]))] * 0.01, color="black",
+    ax.annotate('V_0={:.3f}A^3/atom'.format(eq_vol),
+                xy=(0, -300))
+    plt.axvline(eq_vol - array_2[0][array_2[1].index(min(array_2[1]))] * 0.01, color="black",
                 linestyle='--')
 
-    plt.text(91, -0.0025, "created by Frank Lauerman 2022/05/11")
+    plt.text(0, -100, "created by Frank Lauerman 2022/05/11")
     plt.title("{} Equation of State for {} in DFT {}".format('Murnaghan', chemical_symbol, density_functional_exchange))
     return ax, plt
 
@@ -66,16 +65,16 @@ volumes = linspace(min(array_2[0]), max(array_2[0]), len(fit_eos_curve))
 line1, = ax.plot(array_2[0], array_2[1], 'o')
 line2, = ax.plot(volumes, fit_eos_curve, color="black")
 
-x_min = (min(array_2[0]) - (min(array_2[0]) * 0.10))
-x_max = (max(array_2[0]) + (max(array_2[0]) * 0.10))
-y_min = -537.33  # (min(array_2[1]) - (min(array_2[0]) * 0.00010))
-y_max = 14.49  # (max(array_2[1]) + (max(array_2[0]) * 0.00010))
+x_min = 10
+x_max = 20
+y_min = -537
+y_max = 540
 
 plt.xlim(x_min, x_max)
 plt.ylim(y_min, y_max)
 plt.xlabel(r'$V$ (Å$^3$/atom)')
 plt.ylabel(r'$E$ (eV/atom)')
-bulk_modulus_gpa = convert_units(bulk_modulus, "rb/cb")  # 7
+bulk_modulus_gpa = convert_units(bulk_modulus, "rb/cb")
 eq_vol = array_2[0][array_2[1].index(min(array_2[1]))]
 annotate_graph(chemical_symbol, crystal_symmetry)
 
@@ -87,5 +86,35 @@ if display_graph:
 elif not display_graph:
     plt.savefig("Lauerman.Al.Fm-3m.GGA-PBE.MurnaghanEquationOfState.png")
 
+# ---------------------------------------------------------------------------------------------------------------------#
+from generate_matrix import generate_matrix
+from lowest_eigenvectors import lowest_eigenvectors
+from numpy import linspace
+import matplotlib.pyplot as plt
 
-print()
+display_graph = False
+potential_name = 'harmonic'
+N_dim = 120
+potential_parameter = 100
+
+matrix = generate_matrix(-10, 10, N_dim, potential_name, potential_parameter)
+
+eigenvalues, eigenvectors = lowest_eigenvectors(matrix, 3)
+
+x = linspace(-10, 10, N_dim)
+line1, = plt.plot(x, eigenvectors[0][0:N_dim])
+line2, = plt.plot(x, eigenvectors[1][0:N_dim])
+line3, = plt.plot(x, eigenvectors[2][0:N_dim])
+
+plt.xlabel("x [a.u.]")
+plt.ylabel("ψ n ( x ) [a.u.]")
+plt.legend((line1, line2, line3), ('ψ1, Ε1 = 0.62414396 a.u.', 'ψ2, Ε2 = 0.87335307 a.u.', 'ψ3, Ε3 = 1.12229893 a.u.'))
+plt.axis([-10, 10, max(eigenvectors[0]) - 2, max(eigenvectors[0]) + 2])
+plt.axhline(color="black")
+plt.text(-9.5, -1.75, "Created by Frank Lauerman 2022/05/11")
+plt.title("Select Wavefunctions for a Harmonic Potential on a Spatial Grid of 0, 1, 2 Points")
+
+if display_graph:
+    plt.show()
+elif not display_graph:
+    plt.savefig("Lauerman.harmonic.Eigenvector0, 1, 2.png")
