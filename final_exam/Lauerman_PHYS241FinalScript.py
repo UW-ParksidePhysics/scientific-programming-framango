c5ee7fbd3d3056fd19c3ca6cd38cd940ab810357
framango <framango99@gmail.com>
2022-05-11 13:38:44 -0500

    Quad coe works??

--- final_exam/Lauerman_PHYS241FinalScript.py
+++ final_exam/Lauerman_PHYS241FinalScript.py
@@ -1,14 +1,24 @@
 from two_column_text_read import two_column_text_read
-
-filename = 'Al.Fm-3m.GGA-PBE.volumes_energies.dat'
-
-
-def parse_file_name(filename):
-    x = filename.split(".")
-    chemical_symbol = x[0]
-    crystal_symmetry_symbol = x[1]
-    density_functional_exchange = x[2]
-    return chemical_symbol, crystal_symmetry_symbol, density_functional_exchange
+from bivariate_statistics import bivariate_statistics
+from quadratic_fit import quadratic_fit
+from fit_curve_array import fit_curve_array
+from plot_data_with_fit import plot_data_with_fit
+from equations_of_state import fit_eos
+from numpy import linspace
+import matplotlib.pyplot as plt
 
 
+def parse_file_name(file_name):
+    x = file_name.split(".")
+    symbol = x[0]
+    structure = x[1]
+    acronym = x[2]
+    return symbol, structure, acronym
 
+
+file_name = "Al.Fm-3m.GGA-PBE.volumes_energies.dat"
+symbol, structure, acronym = parse_file_name(file_name)
+array = two_column_text_read("Al.Fm-3m.GGA-PBE.volumes_energies.dat")
+statistics = bivariate_statistics(array)
+quadratic_coefficients = quadratic_fit(array)
+print(quadratic_coefficients)
